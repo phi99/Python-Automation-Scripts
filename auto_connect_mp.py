a7a863b4411577656eef68da54a421b6ae764d83
phi99 <34248917+phi99@users.noreply.github.com>
2019-03-27 12:28:06 +1100

    Update auto_connect_mp.py

--- auto_connect_mp.py
+++ auto_connect_mp.py
@@ -15,38 +15,49 @@ parser=argparse.ArgumentParser(
 # Add Positional Arguments
 parser.add_argument("--INPUT_CMD",nargs='+',default=[], help="Command to be executed")
 parser.add_argument("--TARGET_DEV",nargs='+',default=[],help="Target Device to login to")
+parser.add_argument("--TARGET_DIR",nargs='+',default=[],help="Target Directory of output file")
 
 # Parsing and using the arguments
 args = parser.parse_args()
 
 input_cmd = args.INPUT_CMD
 target_dev = args.TARGET_DEV
+target_dir = args.TARGET_DIR
+
+#convert target directory to string
+str_target_dir=''.join(str(e) for e in target_dir)
 
 processes=[]
-#timestr = time.strftime("%Y%m%d-%H%M%S")
+
+#Get start time
 start_time=datetime.now()
 date_time=start_time.strftime("%Y%m%d-%H%M%S")
 
-def connect_dev(ip):
+#Function to connect to device, run cmd, and save output file
+def connect_dev(ip,input_cmd,str_target_dir):
   sshcmdx='ssh -o StrictHostKeyChecking=no ' + 'root@' + ip
   p=testmod.spawn(sshcmdx)
   for cmd in input_cmd:
     p.expect(".")
     p.sendline(cmd)
-    filename=ip+"_" + date_time + ".log"
+    filename=str_target_dir+ip+"_" + date_time + ".log"
     filename_out=file(filename,'w')
     p.logfile=filename_out
   p.sendline('exit')
   p.interact()
 
+#Spawn new process for each device
 for i in target_dev:
-  processes.append(mp.Process(target=connect_dev, args=[i]))
+  processes.append(mp.Process(target=connect_dev, args=[i,input_cmd,str_target_dir]))
 
-for p in processes:
-  p.start()
+for a in processes:
+  a.start()
 
-for p in processes:
-  p.join()
+for a in processes:
+  a.join()
 
+#Get end time
 end_time=datetime.now()
+
+#Get time it takes to run the script
 print("Script Execution time {}".format(end_time-start_time))
